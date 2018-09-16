0b1632e4903d2ae3f9d8c17ca507ab9132f28a71
yangpeng <252490835@qq.com>
2018-09-16 18:08:50 +0800

    test

--- ys/gopoint.go
+++ ys/gopoint.go
@@ -8,7 +8,23 @@ import "fmt"
 func main() {
 	var a = 20
 	var ia *int
-	fmt.Print("%x\n", &a)
-	fmt.Print("%x\n", &ia)
-	fmt.Print("%x\n", *ia)
+
+	ia = &a
+	fmt.Println("%x\n", &a)
+	fmt.Println("%x\n", &ia)
+	fmt.Println("%x\n", *ia)
+	fmt.Println("_________________________________________")
+	var a2 int = 20 /* 声明实际变量 */
+	var ip *int     /* 声明指针变量 */
+
+	ip = &a /* 指针变量的存储地址 */
+
+	fmt.Printf("a 变量的地址是: %x\n", &a2)
+
+	/* 指针变量的存储地址 */
+	fmt.Printf("ip 变量储存的指针地址: %x\n", ip)
+
+	/* 使用指针访问值 */
+	fmt.Printf("*ip 变量的值: %d\n", *ip)
+
 }
