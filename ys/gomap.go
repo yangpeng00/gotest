59004c3693661d362269a6aa57af0d19f48289a0
ys <1500591043@qq.com>
2018-09-16 18:03:29 +0800

    aa

--- ys/gomap.go
+++ ys/gomap.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 /*
 map的增删改查， 循环
 */
+func main() {
+	var countryMap map[string]string
+	countryMap = make(map[string]string)
+	countryMap["fasd"] = "马裤"
+	countryMap["sfad"] = "十分动感"
+	countryMap["hgljk"] = "官服"
+	countryMap["tyui"] = "额外"
+	for country := range countryMap {
+		fmt.Println(country, "首都是", countryMap[country])
+	}
+	cap, ok := countryMap["美国"]
+	if ok {
+		fmt.Println("美国的首都是", cap)
+	} else {
+		fmt.Println("美国的首都不存在")
+	}
+}
--- ys/gopoint.go
+++ ys/gopoint.go
@@ -6,9 +6,10 @@ import "fmt"
 1.指针的创建使用，指针参数等
 */
 func main() {
-	var a = 20
+	var a int = 20
 	var ia *int
-	fmt.Print("%x\n", &a)
-	fmt.Print("%x\n", &ia)
+	ia = &a
+	fmt.Print(&a)
+	fmt.Print("%x\n", ia)
 	fmt.Print("%x\n", *ia)
 }
