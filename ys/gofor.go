c48ad96524a0bb19bb33aec6bdb24e58e6abd234
ys <1500591043@qq.com>
2018-09-16 17:10:05 +0800

    aa

--- ys/gofor.go
+++ ys/gofor.go
@@ -1,1 +1,1 @@
-package ys
+package main
--- ys/gotype.go
+++ ys/gotype.go
@@ -1,6 +1,19 @@
-package ys
+package main
+
+import "fmt"
 
 /*
 1.go的基本类型的用法
 2.常量，变量的创建使用
 */
+func main() {
+
+	var a = 31
+	var aa, b, c, d int = 1, 2, 3, 4
+	var (
+		bb int
+		cc int
+		dd bool
+	)
+	fmt.Print(a, aa, b, c, d, bb, cc, dd)
+}
